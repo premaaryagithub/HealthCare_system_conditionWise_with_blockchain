231aae1f3197392fdd1a935ab240dd4f17c800ef
premaaryagithub <147481961+premaaryagithub@users.noreply.github.com>
2026-06-06 09:42:12 +0530

    Delete extract_docx.py

--- extract_docx.py
+++
@@ -1,7 +0,0 @@
-import docx
-def extract():
-    doc = docx.Document('sample.docx')
-    with open('sample_docx_text.txt', 'w', encoding='utf-8') as f:
-        for para in doc.paragraphs:
-            f.write(para.text + '\n')
-extract()
